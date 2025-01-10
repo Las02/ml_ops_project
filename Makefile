5b1f5f636d55cbb14f7167324cbd9f99e495e2c7
las02 <lasse101010@gmail.com>
2025-01-10 12:50:47 +0100

    .

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ download:
 	python src/ml_ops_project/data.py --download-data
 help:
 	python src/ml_ops_project/data.py --help
+test: 
+	pytest
--- src/ml_ops_project/data.py
+++ src/ml_ops_project/data.py
@@ -1,22 +1,44 @@
 from pathlib import Path
 
+import torch
 import typer
 from datasets import load_dataset
+from loguru import logger
 from torch.utils.data import Dataset
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("t5-small")
 
 app = typer.Typer()
 
-# class MyDataset(Dataset):
-#    """My custom dataset."""
-#
-#    def __init__(self, raw_data_path: Path) -> None:
-#        self.data_path = raw_data_path
-#
-#    def __len__(self) -> int:
-#        """Return the length of the dataset."""
-#
-#    def __getitem__(self, index: int):
-#        """Return a given sample from the dataset."""
+
+class preprocess_data:
+    """preprocess data"""
+
+    def __init__(self, preprocess_data_path: Path) -> None:
+        self.data_path = preprocess_data_path
+        self.danish, self.english = self.read_in_file(preprocess_data_path)
+
+    def read_in_file(self, preprocess_data_path: Path):
+        danish_all = []
+        english_all = []
+        with open(preprocess_data_path, "r") as f:
+            for line in f:
+                line = line.split("###>")
+                if len(line) != 2:
+                    logger.critical(f"line in dataset has more or less than two language entries: {line}")
+                danish = line[0].strip()
+                english = line[1].strip()
+                danish_all.append(danish)
+                english_all.append(english)
+        return (danish_all, english_all)
+
+
+dl = Opus_datalaoder("./data/raw/train.txt")
+# dl.danish
+# %%
+
+
 #
 #    def preprocess(self, output_folder: Path) -> None:
 #         """Preprocess the raw data and save it to the output folder."""
