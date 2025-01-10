68a95318d4ad198dcf97b39f90818c8d18048362
Benedictemaul <s203581@dtu.dk>
2025-01-10 12:15:54 +0100

    download_data feature

--- src/ml_ops_project/data.py
+++ src/ml_ops_project/data.py
@@ -2,28 +2,42 @@ from pathlib import Path
 
 import typer
 from torch.utils.data import Dataset
+from datasets import load_dataset
 
+app = typer.Typer()
 
-class MyDataset(Dataset):
-    """My custom dataset."""
+#class MyDataset(Dataset):
+#    """My custom dataset."""
+#
+#    def __init__(self, raw_data_path: Path) -> None:
+#        self.data_path = raw_data_path
+#
+#    def __len__(self) -> int:
+#        """Return the length of the dataset."""
+#
+#    def __getitem__(self, index: int):
+#        """Return a given sample from the dataset."""
+#
+#    def preprocess(self, output_folder: Path) -> None:
+#         """Preprocess the raw data and save it to the output folder."""
 
-    def __init__(self, raw_data_path: Path) -> None:
-        self.data_path = raw_data_path
+# def preprocess(raw_data_path: Path, output_folder: Path) -> None:
+#     print("Preprocessing data...")
+#     dataset = MyDataset(raw_data_path)
+#     dataset.preprocess(output_folder)
 
-    def __len__(self) -> int:
-        """Return the length of the dataset."""
+@app.command()  
+def download_data():
+    ds = load_dataset("kaitchup/opus-Danish-to-English")
+    
+    with open("data/raw/train.txt", "w") as f:
+        for line in ds["train"]["text"]:
+            print(line, file=f)
 
-    def __getitem__(self, index: int):
-        """Return a given sample from the dataset."""
-
-    def preprocess(self, output_folder: Path) -> None:
-        """Preprocess the raw data and save it to the output folder."""
-
-def preprocess(raw_data_path: Path, output_folder: Path) -> None:
-    print("Preprocessing data...")
-    dataset = MyDataset(raw_data_path)
-    dataset.preprocess(output_folder)
-
+    with open("data/raw/validation.txt", "w") as f:
+        for line in ds["validation"]["text"]:
+            print(line, file=f)
 
 if __name__ == "__main__":
-    typer.run(preprocess)
+    app()
+    #typer.run(download_data)
