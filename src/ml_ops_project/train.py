725f6393ff8c34cc4ff7554e30a06d3dc6856480
las02 <lasse101010@gmail.com>
2025-01-16 16:42:31 +0100

    BEFORE BENED

--- src/ml_ops_project/train.py
+++ src/ml_ops_project/train.py
@@ -1,10 +1,13 @@
+import torch
 import torch.nn.functional as F
 import typer
 import yaml
 from loguru import logger
+from torch.optim import AdamW
 
 # %%
 from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
 from transformers import (
     AutoModelForSeq2SeqLM,
     Seq2SeqTrainer,
@@ -13,21 +16,10 @@ from transformers import (
     T5Tokenizer,
 )
 
+import wandb
 from ml_ops_project.data import OpusDataset, Tokenize_data
 from ml_ops_project.model import *
 from ml_ops_project.model import initialize_model, load_model_config
-from tqdm.auto import tqdm
-import torch 
-from torch.optim import AdamW
-
-# Load Model
-model = initialize_model(load_model_config())
-
-# Set Device
-device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
-
-# Set optimizer
-optimizer = AdamW(model.parameters(), lr=5e-5)
 
 # Load Data
 app = typer.Typer()
@@ -37,57 +29,62 @@ app = typer.Typer()
 def none():
     pass
 
-@app.command()
 
+@app.command()
 def train(num_epochs: int = 10):
+    # Load Model
+    model = initialize_model(load_model_config())
+
+    # Set Device
+    device = torch.device(
+        "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
+    )
+
+    # Set optimizer
+    optimizer = AdamW(model.parameters(), lr=5e-5)
     config = load_model_config()
     model = initialize_model(config)
 
     dataset = OpusDataset("data/test_data/test_data.txt")
     train_dataloader = DataLoader(dataset, batch_size=2, shuffle=False)
 
-    model.train()
-    for epoch in tqdm(range(num_epochs)):
-        for truth, input in train_dataloader:
-            outputs = model(input_ids=input, labels=truth)
-            preds = F.softmax(outputs.logits, dim=-1).argmax(dim=-1)
-
-            loss = outputs.loss
-            loss.backward()
-
-            optimizer.step()
-            optimizer.zero_grad()
-
-            # Remove "<pad>" from preds
-            preds_decoded = dataset.decode(preds)
-            preds_decoded = [pred.replace("<pad>", "") for pred in preds_decoded]
-            print(preds_decoded)
-
-            logger.info("TRAIN SUCCESS")
-            break
+    with wandb.init(
+        # set the wandb project where this run will be logged
+        project="my-awesome-project",
+        # track hyperparameters and run metadata
+        config={
+            "learning_rate": 0.02,
+            "epochs": 10,
+        },
+    ):
+        config = wandb.config
+        for epoch in tqdm(range(config.epochs)):
+            wandb.log(
+                {
+                    "epoch": epoch,
+                }
+            )
+            train_epoch(model, optimizer, dataset, train_dataloader)
 
 
-# %%
+def train_epoch(model, optimizer, dataset, dataloader):
+    for truth, input in dataloader:
+        outputs = model(input_ids=input, labels=truth)
+        preds = F.softmax(outputs.logits, dim=-1).argmax(dim=-1)
 
-#
-# # Import Configuration
-# def load_training_config(config_path="/Users/frederikreimert/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/Kandidat_DTU/2024E/MLops/project_folder/ml_ops_project/configs/train/train_config.yaml"):
-#     with open(config_path, "r") as file:
-#         config_dict = yaml.safe_load(file)
-#     return Seq2SeqTrainingArguments(**config_dict)
-#
-# training_args = load_training_config()
-#
-# # Load Trainer
-# trainer = Seq2SeqTrainer(
-#     model=model,
-#     args=training_args,
-#     train_dataset=tokenized_books["train"],
-#     eval_dataset=tokenized_books["test"],
-#     processing_class=tokenizer,
-#     data_collator=data_collator,
-#     compute_metrics=compute_metrics,
-# )
+        loss = outputs.loss
+        loss.backward()
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+        # Remove "<pad>" from preds
+        preds_decoded = dataset.decode(preds)
+        preds_decoded = [pred.replace("<pad>", "") for pred in preds_decoded]
+        print(preds_decoded)
+
+        logger.info("TRAIN SUCCESS")
+        wandb.log({"loss": loss})
 
 
 if __name__ == "__main__":
