b92551263d44db1b3fccea36ea18dd2a1f3977e2
frederikreimert <frederikreimert@gmail.com>
2025-01-16 10:48:50 +0100

    train.py og configuration til den er tilføjet

---
+++ src/ml_ops_project/train.py
@@ -0,0 +1,21 @@
+import yaml
+from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
+
+# Import Configuration
+def load_training_config(config_path="/Users/frederikreimert/Library/CloudStorage/OneDrive-DanmarksTekniskeUniversitet/Kandidat_DTU/2024E/MLops/project_folder/ml_ops_project/configs/train/train_config.yaml"):
+    with open(config_path, "r") as file:
+        config_dict = yaml.safe_load(file)
+    return Seq2SeqTrainingArguments(**config_dict)
+
+training_args = load_training_config()
+
+# Load Trainer
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_books["train"],
+    eval_dataset=tokenized_books["test"],
+    processing_class=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
