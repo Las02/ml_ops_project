1857062610bc90f2851ac0e07ab7a19d382f679c
las02 <lasse101010@gmail.com>
2025-01-23 14:18:28 +0100

    .

--- scratch.py
+++
@@ -1,5 +0,0 @@
-from transformers import T5ForConditionalGeneration, T5Tokenizer
-
-tokenizer = T5Tokenizer.from_pretrained("google-t5/t5-small")
-model = T5ForConditionalGeneration.from_pretrained("google-t5/t5-small")
-model(input_ids=input_ids, labels=labels)
