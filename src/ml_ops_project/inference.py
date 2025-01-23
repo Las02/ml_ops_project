7c22b9f6528396d71259dccd0059c9dded282e0b
fvhreimert <62186760+fvhreimert@users.noreply.github.com>
2025-01-23 11:05:35 +0000

    Pre-commit fixes

--- src/ml_ops_project/inference.py
+++ src/ml_ops_project/inference.py
@@ -2,30 +2,32 @@ import torch
 from transformers import pipeline, T5Tokenizer, T5ForConditionalGeneration
 from tokenizers.normalizers import Sequence, Replace, Lowercase
 
-def translate_danish_to_english(input_text: str):
 
-    #Load the base T5 model
+def translate_danish_to_english(input_text: str):
+    # Load the base T5 model
     model = T5ForConditionalGeneration.from_pretrained("google-t5/t5-small")
 
-    #Load your custom fine-tuned weights
+    # Load your custom fine-tuned weights
     model.load_state_dict(torch.load("models/model.pt", map_location="cpu"))
     model.eval()
 
-    #Load the matching tokenizer
+    # Load the matching tokenizer
     tokenizer = T5Tokenizer.from_pretrained("google-t5/t5-small")
 
-    normalizer = Sequence([
-        Replace("å", "aa"),
-        Replace("ø", "oe"),
-        Replace("æ", "ae"),
-        Lowercase(),
-    ])
+    normalizer = Sequence(
+        [
+            Replace("å", "aa"),
+            Replace("ø", "oe"),
+            Replace("æ", "ae"),
+            Lowercase(),
+        ]
+    )
     tokenizer.normalizer = normalizer
 
     if model.config.decoder_start_token_id is None:
         model.config.decoder_start_token_id = tokenizer.pad_token_id
 
-    #Build a pipeline for translation da->en
+    # Build a pipeline for translation da->en
     translator = pipeline(
         task="translation_da_to_en",
         model=model,
@@ -33,7 +35,7 @@ def translate_danish_to_english(input_text: str):
         device=-1,  # run on CPU
     )
 
-    #Translate
+    # Translate
     result = translator(input_text, max_length=240)
 
     # result is a list of dicts [{'translation_text': '...'}]
